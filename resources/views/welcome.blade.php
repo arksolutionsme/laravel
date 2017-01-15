ff31022bb05ec5fb70fd1e66bbae8993602f9474
Mark Timbol <mark.timbol@hotmail.com>
2017-01-15 18:01:05 +0400

    Add Algolia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -88,6 +88,7 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://algolia.com">Algolia</a>
                     <a href="http://arksolutions.me">By Ark Solutions LLC</a>
                 </div>
             </div>
